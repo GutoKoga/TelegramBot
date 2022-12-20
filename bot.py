ca43462636f1d2c5cc01595e8b94f3c666a8a358
GutoKoga <github@gutokoga.com.br>
2022-12-20 14:13:03 -0300

    New

--- bot.py
+++ bot.py
@@ -1,32 +1,12 @@
-'''
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-async def send_welcome(message):
-    await bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
-
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-async def echo_message(message):
-    await bot.reply_to(message, message.text)
-
-
-import asyncio
-asyncio.run(bot.polling())
-'''
-
 import os
-from telebot.async_telebot import AsyncTeleBotimport
+import telebot
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chave = os.getenv('TOKEN')
 
-bot = AsyncTeleBot(chave, parse_mode="HTML")
+bot = telebot.TeleBot(chave, parse_mode="HTML")
 
 textol1 = """</b><code>, como eu posso te ajudar?
 
@@ -66,9 +46,9 @@ textol3 = """</b><code>, segue nossos contatos:
 """
 
 textol4 = """</b><code>, tenha um chat bot para te auxiliar no relacionamento com seu cliente.
- 
+
 Automatize as respostas para as perguntas mais frequentes.
- 
+
 Tenha um atendimento 24 horas. 
 
 Entre em </code>/contato <code> para criarmos um orçamento personalizado.</code>
@@ -115,43 +95,52 @@ Entre em </code>/contato <code> para criarmos um orçamento personalizado.</code
 🔙  /start <code>=> Voltar</code>
 """
 
+
 @bot.message_handler(commands=["start"])
-async def start(mensagem):
-  await bot.send_message(mensagem.chat.id, "<code>Olá </code><b>" + mensagem.from_user.first_name + textol1)
+def start(mensagem):
+    bot.send_message(mensagem.chat.id, "<code>Olá </code><b>" + mensagem.from_user.first_name + textol1)
+
 
 @bot.message_handler(commands=["portfolio"])
-async def portfolio(mensagem):
-  await bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol2)
+def portfolio(mensagem):
+    bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol2)
+
 
 @bot.message_handler(commands=["contato"])
-async def contato(mensagem):
-  await bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol3)
+def contato(mensagem):
+    bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol3)
+
 
 @bot.message_handler(commands=["chatbot"])
-async def chatbot(mensagem):
-  await bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol4)
+def chatbot(mensagem):
+    bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol4)
+
 
 @bot.message_handler(commands=["site"])
-async def site(mensagem):
-  await bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol5)
+def site(mensagem):
+    bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol5)
+
 
 @bot.message_handler(commands=["appweb"])
-async def appweb(mensagem):
-  await bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol6)
+def appweb(mensagem):
+    bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol6)
+
 
 @bot.message_handler(commands=["appmobile"])
-async def appmobile(mensagem):
-  await bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol7)
+def appmobile(mensagem):
+    bot.send_message(mensagem.chat.id, "<b>" + mensagem.from_user.first_name + textol7)
 
-async def verificar(mensagem):
-  return True
+
+def verificar(mensagem):
+    return True
+
 
 @bot.message_handler(func=verificar)
-async def responder(mensagem):
-  await bot.reply_to(
-    mensagem, mensagem.from_user.first_name +
-    ", não utilize o teclado, clique apenas nas opções que aparecem nas mensagens, precedidas por uma barra."
-  )
+def responder(mensagem):
+    bot.reply_to(
+        mensagem, mensagem.from_user.first_name +
+                  ", não utilize o teclado, clique apenas nas opções que aparecem nas mensagens, precedidas por uma barra."
+    )
 
-import asyncio
-asyncio.run(bot.polling())
+
+bot.infinity_polling(timeout=10, long_polling_timeout=5)
